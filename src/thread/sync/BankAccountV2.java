607942f477dffe86da6e5129e93687cfbc54ab7d
강경지 <kangkj001@gmail.com>
2025-04-20 16:25:57 +0900

    20250420

--- src/thread/sync/BankAccountV2.java
+++ src/thread/sync/BankAccountV2.java
@@ -5,36 +5,36 @@ import static util.ThreadUtils.sleep;
 
 public class BankAccountV2 implements BankAccount{
 
-    volatile private int balance; //잔고
+     private int balance; //잔고
 
     public BankAccountV2(int initialBalance) {
         this.balance = initialBalance;
     }
 
     @Override
-    public boolean withdraw(int amount) {
+    public synchronized boolean withdraw(int amount) {
         log("거래 시작: "+ getClass().getSimpleName());
-        //잔고가 출금액 보다 적으면, 진행하면 안됨
 
+
+        // 임계 영역 시작
         log("[검증 시작] 출금액: "+amount +", 잔액: "+balance);
         if (balance<amount){
             log("[검증 실패]");
             return false;
         }
-
         //잔고가 출금액 보다 많으면, 진행
         log("[검증 완료] 출금액: "+amount+", 잔액: "+balance);
         sleep(1000); //출금에 걸리는 시간으로 가정
         balance  -= amount;
         log("[출금 완료] 출금액: "+amount+", 잔액: "+balance);
-
+        // ==임계 영역 종료==
 
         log("거래 종료 ");
         return true;
     }
 
     @Override
-    public int getBalance() {
+    public synchronized int getBalance() {
         return balance;
     }
 }
